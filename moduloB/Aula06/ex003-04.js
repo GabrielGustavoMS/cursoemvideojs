fc8e5358507e82cea1f90536f4c1a77303699187
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2022-04-28 11:05:29 -0300

    ex003-04
    Modificando o exercicio anterior para usar template string

---
+++ moduloB/Aula06/ex003-04.js
@@ -0,0 +1,5 @@
+// Javascript
+var numb1 = Number.parseFloat(window.prompt('Digite um número'))
+var numb2 = Number.parseFloat(window.prompt('Digite outro número'))
+//Usando template string
+window.alert(`O resultado da adição é: ${numb1} + ${numb2}`)
