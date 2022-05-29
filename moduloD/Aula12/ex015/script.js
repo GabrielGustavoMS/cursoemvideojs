031a78b4e5141ec602e3a6fdf0498de48ae8e38a
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2022-05-29 17:04:44 -0300

    Exercício 015

--- moduloD/Aula12/ex015/script.js
+++ moduloD/Aula12/ex015/script.js
@@ -14,10 +14,30 @@ function verificar() {
     img.setAttribute('id', 'foto')
     if (fsex[0].checked) {
       gênero = 'Homem'
-    } else {
+      if (idade < 10) {
+        img.setAttribute('src', './img/menino.jpg')
+      } else if (idade < 25) {
+        img.setAttribute('src', './img/jovem-m.jpg')
+      } else if (idade < 50) {
+        img.setAttribute('src', './img/homem.jpg')
+      } else {
+        img.setAttribute('src', './img/idoso.jpg')
+      }
+    } else if (fsex[1].checked) {
       gênero = 'Mulher'
+      if (idade < 10) {
+        img.setAttribute('src', './img/menina.jpg')
+      } else if (idade < 25) {
+        img.setAttribute('src', './img/jovem-f.jpg')
+      } else if (idade < 50) {
+        img.setAttribute('src', './img/mulher.jpg')
+      } else {
+        img.setAttribute('src', './img/idosa.jpg')
+      }
     }
     res.style.textAlign = 'center'
-    res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos.<br/>`
+    res.appendChild(img)
+    res.innerHTML += `<br/><small><em>Fonte da Imagem: Pexels.com</em></small>`
   }
 }
